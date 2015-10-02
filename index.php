511c0224f10fd60f4e12c360653c7bd04f0c691c
Rizza Mendoza <mendoza.rizza@gmail.com>
2015-10-02 15:35:25 +0800

    Included amount

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 $command = $_GET['command'];
 $text = $_GET['text'];
 
+$amount = 1;
 $from = "";
 $to = "";
 
@@ -16,13 +17,21 @@ $text = str_replace("to", "", $text);
 
 //get initial currency
 $from = trim($text);
+if (strpos($from, " ") !== FALSE)
+{
+    $from_pos = strrpos($from, " ");
+    $from = trim(substr($from, $from_pos, 4));
+    $text = trim(str_replace($from, "", $text));
+    
+    $amount = $text;
+}
 
 //connect to Google currency converter
-$get = file_get_contents('https://www.google.com/finance/converter?a=1&from='.$from.'&to='.$to);
+$get = file_get_contents('https://www.google.com/finance/converter?a='.$amount.'&from='.$from.'&to='.$to);
 $get = explode("<span class=bld>",$get);
 $get = explode("</span>",$get[1]);  
 $converted_amount = preg_replace("/[^0-9\.]/", null, $get[0]);
 
-echo "1 ".strtoupper($from)." = ".$converted_amount." ".strtoupper($to);
+echo $amount." ".strtoupper($from)." = ".$converted_amount." ".strtoupper($to);
 
 ?>
